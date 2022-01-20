b01ff073b62e6a4f748d26c29c5ff5d08d95c608
wangkalbi-gernessen <guizhou08chishui@gmail.com>
2022-01-20 12:04:48 -0800

    Complete to display a List

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from 'src/hero';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -8,13 +9,21 @@ import { Hero } from 'src/hero';
 })
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1,
-    name: "Windstorm"
-  }
+  // hero: Hero = {
+  //   id: 1,
+  //   name: "Windstorm"
+  // }
+
+  heroes = HEROES;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
